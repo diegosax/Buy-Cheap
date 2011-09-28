8356c770bb45f8226816bbc716d38b86910e45c4
Diego Marcolan <diegosaxbat@gmail.com>
2011-09-28 13:45:41 -0300

    new message

--- db/migrate/20110215150818_remove_user_id_from_address.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserIdFromAddress < ActiveRecord::Migration
-  def self.up
-    remove_column :addresses, :user_id
-  end
-
-  def self.down
-    add_column :addresses, :user_id, :integer
-  end
-end
