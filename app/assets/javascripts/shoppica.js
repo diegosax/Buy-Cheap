cd2e011936d745ea70de1ee4474ec54e2c6e2a1a
Diego Marcolan <diegosax@terra.com.br>
2011-12-22 21:01:51 -0300

    Tryed to fix products hover

--- app/assets/javascripts/shoppica.js
+++ app/assets/javascripts/shoppica.js
@@ -3,12 +3,12 @@ jQuery( function($) {
   var site_options = {
     'main_color'      : '#71b013',
     'secondary_color' : '#ff9900'      
-  };
+  }
 
   // Hover effect for the header menu
   $("#categories > ul > li").not("#menu_home").hover(
     function() {
-        $(this).find("a:first").stop().animate({
+      $(this).find("a:first").stop().animate({
           color: '#ffffff',
           backgroundColor: site_options.secondary_color
         },300
@@ -24,7 +24,20 @@ jQuery( function($) {
     }
   );
 
-  
+  if (!$.browser.msie || parseInt($.browser.version, 10) > 8) {
+      var onMouseOutOpacity = 1;
+      $('div.s_listing > div.s_item').css('opacity', onMouseOutOpacity)
+        .hover(
+          function () {
+            $(this).prevAll().stop().fadeTo('slow', 0.60);
+            $(this).nextAll().stop().fadeTo('slow', 0.60);
+          },
+          function () {
+            $(this).prevAll().stop().fadeTo('slow', onMouseOutOpacity);
+            $(this).nextAll().stop().fadeTo('slow', onMouseOutOpacity);
+          }
+      );
+  }
 
   // Hover effect for the shoppica cart
   $("#cart_menu").hover(
@@ -44,5 +57,25 @@ jQuery( function($) {
       );
     }
   );
+
+  // Animation for the languages and currency dropdown
+  $('.s_switcher').hover(function() {
+    $(this).find('.s_options').stop(true, true).slideDown('fast');
+  },function() {
+    $(this).find('.s_options').stop(true, true).slideUp('fast');
+  });
+
+  var search_visibility = 0;
+  // Animation for the search button
+  $("#show_search").bind("click", function(){
+    if (search_visibility == 0) {
+      $("#search_bar").fadeIn();
+      search_visibility = 1;
+    } else {
+      $("#search_bar").fadeOut();
+      search_visibility = 0;
+    }
+  });
+
 
 });
