d87fc2e5b2e8966684fbc2e02553d546f6e77efd
Diego Marcolan <diegosax@terra.com.br>
2011-12-28 11:18:44 -0300

    Fixing most sold at heroku

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,5 +1,5 @@
 module DashboardHelper
 	def most_sold_products 
-		@products = Product.find_by_sql("select *, P.id from line_items L JOIN Products P ON L.product_id = P.id Group By P.id Order By SUM(quantity) DESC")
+		Product.joins(:line_items)
 	end
 end
